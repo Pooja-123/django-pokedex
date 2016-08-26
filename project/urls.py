d4b3ce87248691ad782c26084f2dd5df07dafde9
bwillis32 <bwillis@bentoncorp.com>
2016-08-26 09:31:20 -0400

    commit migration

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/', include('pokedex.urls'))
+    url(r'^api/v1/', include(r'pokedex.urls'))
 ]
